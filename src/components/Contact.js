8bc1e28298f46f8fb4787234da6f59f380665828
1KELER1 <123807688+1KELER1@users.noreply.github.com>
2025-02-05 19:30:18 +0800

    fon

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 function Contact() {
   return (
-    <section className="contact">
-      <h2>Контакты</h2>
-      <p>Телефон: <a href="tel:+78001234567">(800) 123-45-67</a></p>
-      <p>Email: <a href="mailto:eco@angtu.ru">eco@angtu.ru</a></p>
-    </section>
+    <section class="contact">
+  <h2>Контакты</h2>
+  <p>Телефон: <a href="tel:+78001234567">(800) 123-45-67</a></p>
+  <p>Электронная почта: <a href="mailto:eco@angtu.ru">eco@angtu.ru</a></p>
+</section>
+
   );
 }
 
