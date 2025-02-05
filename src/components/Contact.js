a04ee9f39b513a38bfc79c5640eccc67580f4704
1KELER1 <123807688+1KELER1@users.noreply.github.com>
2025-02-05 18:51:12 +0800

    1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,12 +4,8 @@ function Contact() {
   return (
     <section className="contact">
       <h2>Контакты</h2>
-      <p>Телефон: (800) 123-45-67</p>
-      <p>Email: eco@angtu.ru</p>
-      <form>
-        <input type="email" placeholder="Ваш email" />
-        <button type="submit">Отправить</button>
-      </form>
+      <p>Телефон: <a href="tel:+78001234567">(800) 123-45-67</a></p>
+      <p>Email: <a href="mailto:eco@angtu.ru">eco@angtu.ru</a></p>
     </section>
   );
 }
